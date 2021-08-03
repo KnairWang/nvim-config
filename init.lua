defd52fbc19c78dbb4d639c0e5f540e1ce2766df
Cangsong WANG <cwang40@slb.com>
2021-08-03 13:37:58 +0800

    use packer

---
+++ init.lua
@@ -0,0 +1,17 @@
+local cmd = vim.cmd -- to execute Vim commands e.g. cmd('pwd')
+local fn = vim.fn -- to call Vim functions e.g. fn.bufnr()
+local g = vim.g -- a table to access global variables
+local opt = vim.opt -- to set options
+
+require('plugins')
+
+vim.cmd 'set gfn=Hack\\ NF:h14'
+
+vim.cmd 'syntax on'
+vim.cmd 'syntax enable'
+
+vim.cmd 'set nocompatible'
+
+vim.cmd 'set number'
+
+vim.cmd 'set tabstop=1 softtabstop=0 expandtab shiftwidth=4 smarttab'
