10164edab4668a78922ac16636693acefb7d40f5
Knair WANG <knairwang@outlook.com>
2021-08-03 23:09:54 +0800

    update term

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -56,37 +56,32 @@ require('packer').startup(function()
             -- Use (s-)tab to:
             --- move to prev/next item in completion menuone
             --- jump to prev/next snippet's placeholder
-            _G.tab_complete = function()
-                if vim.fn.pumvisible() == 1 then
-                    return t "<C-n>"
-                elseif check_back_space() then
-                    return t "<Tab>"
-                else
-                    return vim.fn['compe#complete']()
-                end
-            end
-            _G.s_tab_complete = function()
-                if vim.fn.pumvisible() == 1 then
-                    return t "<C-p>"
-                else
-                    return t "<S-Tab>"
-                end
-            end
+            -- _G.tab_complete = function()
+                -- if vim.fn.pumvisible() == 1 then
+                    -- return t "<C-n>"
+                -- elseif check_back_space() then
+                    -- return t "<Tab>"
+                -- else
+                    -- return vim.fn['compe#complete']()
+                -- end
+            -- end
+            -- _G.s_tab_complete = function()
+                -- if vim.fn.pumvisible() == 1 then
+                    -- return t "<C-p>"
+                -- else
+                    -- return t "<S-Tab>"
+                -- end
+            -- end
 
-            vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()",
-            {expr = true})
-            vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()",
-            {expr = true})
-            vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()",
-            {expr = true})
-            vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()",
-            {expr = true})
+            -- vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+            -- vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+            -- vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+            -- vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
             -- This line is important for auto-import
-            vim.api.nvim_set_keymap('i', '<cr>', 'compe#confirm("<cr>")',
-            {expr = true})
-            vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()',
-            {expr = true})
+            vim.api.nvim_set_keymap('i', '<Tab>', 'compe#confirm("<cr>")', {expr = true})
+            vim.api.nvim_set_keymap('i', '<cr>', 'compe#confirm("<cr>")', {expr = true})
+            vim.api.nvim_set_keymap('i', '<c-space>', 'compe#complete()', {expr = true})
         end
     }
 
@@ -230,6 +225,34 @@ require('packer').startup(function()
 
     -- cursorline
     use {'yamatsum/nvim-cursorline', requires = {'dracula'}}
+
+    use {
+        'numtostr/FTerm.nvim',
+        config = function()
+            local term = require('Fterm.terminal')
+            local sh = term:new():setup({
+                cmd = 'sh.exe',
+                dimensions  = {
+                    height = 0.8,
+                    width = 0.8,
+                    x = 0.5,
+                    y = 0.5
+                },
+                border = 'double' -- or 'single'
+            })
+
+            function _G.__fterm_sh_toggle()
+                sh:toggle()
+            end
+            -- vim.api.nvim_set_keymap("n", "<C-`>", "v:lua.__fterm_sh_toggle()", {expr = true})
+            --
+            local map = vim.api.nvim_set_keymap
+            local opts = { noremap = true, silent = true }
+
+            map('n', '<C-`>', '<CMD>lua __fterm_sh_toggle()<CR>', opts)
+            map('t', '<C-`>', '<C-\\><C-n><CMD>lua __fterm_sh_toggle()<CR>', opts)
+        end
+    }
 
 end)
 
