6eb111ad0b57c5823e9065a7ca7746476fa708db
Cangsong WANG <cwang40@slb.com>
2021-08-06 16:51:42 +0800

    clean up

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -350,13 +350,6 @@ require('packer').startup(function()
             require("toggleterm").setup{
                 -- size can be a number or function which is passed the current terminal
                 size = 20,
-                -- | function(term)
-                -- if term.direction == "horizontal" then
-                -- return 15
-                -- elseif term.direction == "vertical" then
-                -- return vim.o.columns * 0.4
-                -- end
-                -- end,
                 open_mapping = [[<c-`>]],
                 hide_numbers = true, -- hide the number column in toggleterm buffers
                 shade_filetypes = {},
@@ -368,21 +361,6 @@ require('packer').startup(function()
                 direction = 'float', -- 'vertical' | 'horizontal' | 'window' | 'float',
                 close_on_exit = true, -- close the terminal window when the process exits
                 shell = 'powershell.exe', -- change the default shell
-                -- This field is only relevant if direction is set to 'float'
-                -- float_opts = {
-                -- -- The border key is *almost* the same as 'nvim_win_open'
-                -- -- see :h nvim_win_open for details on borders however
-                -- -- the 'curved' border is a custom border type
-                -- -- not natively supported but implemented in this plugin.
-                -- border = 'single', -- 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-                -- width = 1,
-                -- height = 1,
-                -- winblend = 3,
-                -- highlights = {
-                -- border = "Normal",
-                -- background = "Normal",
-                -- }
-                -- }
             }
         end
     }
