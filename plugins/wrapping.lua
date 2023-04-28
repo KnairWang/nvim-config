a01b77c89d423ffd0cc0301bebe5d8997286d5c3
Cangsong Wang <cwang40@slb.com>
2023-04-28 18:35:33 +0800

    add wrapping

---
+++ plugins/wrapping.lua
@@ -0,0 +1,21 @@
+local opts = {
+  auto_set_mode_filetype_allowlist = {},
+  auto_set_mode_filetype_denylist = {
+    "neo-tree",
+  },
+  auto_set_mode_heuristically = false,
+  create_keymappings = false,
+  create_commands = false,
+  notify_on_switch = false,
+}
+
+return {
+  "andrewferrier/wrapping.nvim",
+  enabled = true,
+  event = "BufEnter",
+  config = function()
+    local wrapping = require("wrapping")
+    wrapping.setup(opts)
+    wrapping.soft_wrap_mode()
+  end
+}
