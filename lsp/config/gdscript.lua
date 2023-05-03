84be38913fc020ee2ddaf8fe7e0bc7c22c6d7933
Cangsong WANG <knairwang@outlook.com>
2023-05-03 19:42:09 +0800

    update gdscript

--- lsp/config/gdscript.lua
+++ lsp/config/gdscript.lua
@@ -1,8 +1,15 @@
 local util = require 'lspconfig.util'
 
 local port = os.getenv 'GDScript_Port' or '6005'
+local address = '127.0.0.1'
 -- local cmd = { 'nc', 'localhost', port }
-local cmd = vim.lsp.rpc.connect('127.0.0.1', tonumber(port))
+local cmd = nil
+
+if ("Windows_NT" == vim.loop.os_uname().sysname) then
+  cmd = { 'ncat', address, port }
+else
+  cmd = vim.lsp.rpc.connect(address, tonumber(port))
+end
 
 return {
   cmd = cmd,
