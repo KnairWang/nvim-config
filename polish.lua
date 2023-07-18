72522cae981b893fb7e927ce0cdb7d214df03d88
Cangsong Wang <cwang40@slb.com>
2023-07-18 10:49:01 +0800

    update

--- polish.lua
+++ polish.lua
@@ -1,4 +1,44 @@
 return function()
+  local uname = vim.loop.os_uname()
+  -- Linux
+  -- Windows
+  local sysname = uname['sysname']
+
+  -- 
+  -- Windows 11 Enterprise
+  local version = uname['version']
+
+  -- 6.1.21.2-microsoft-standard-WSL2
+  -- 10.0.22621
+  local release = uname['release']
+
+  -- x86_64
+  local machine = uname['machine']
+
+  if sysname == 'Linux' or sysname == 'linux' then
+    if string.find(release, 'microsoft') or string.find(release, 'Microsoft') then
+      -- setup for wsl
+      
+    --   if vim.g.clipboard == nil then
+    --     vim.g.clipboard = {}
+    --   end
+    --   local clipboard = vim.g.clipboard
+    --   clipboard['name'] = 'WslClipboard'
+
+    --   clipboard['copy'] = {}
+    --   local copy_cmd = 'clip.exe'
+    --   clipboard['copy']['+'] = copy_cmd
+    --   clipboard['copy']['*'] = copy_cmd
+
+    --   clipboard['paste'] = {}
+    --   local paste_cmd = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).ToString().Replace("`r", "")'
+    --   clipboard['paste']['+'] = paste_cmd
+    --   clipboard['paste']['*'] = paste_cmd
+
+    --   clipboard['cache_enabled'] = 0
+    end
+  end
+
   -- require("neo-tree").setup({
   --     filesystem = { filtered_items = { visible = true } }
   -- })
