69a62a828f4f2113d2a1c427d79eb6cd43f1b1fe
Cangsong Wang <knairwang@outlook.com>
2023-11-25 11:26:59 +0800

    update autopairs

--- plugins/autopairs.lua
+++ plugins/autopairs.lua
@@ -2,16 +2,19 @@ return {
   "windwp/nvim-autopairs",
   opts = {
     ignored_next_char = "[%w%.%(%{%[]",
+    fast_wrap = {
+      chars = { "{", "[", "(", '"', "'" },
+    }
   },
   config = function(plugin, opts)
     require "plugins.configs.nvim-autopairs"(plugin, opts)
 
-    local npairs = require "nvim-autopairs"
-    local Rule = require "nvim-autopairs.rule"
-    local cond = require "nvim-autopairs.conds"
+    -- local npairs = require "nvim-autopairs"
+    -- local Rule = require "nvim-autopairs.rule"
+    -- local cond = require "nvim-autopairs.conds"
 
-    npairs.add_rules(
-      { Rule("<", ">") }
-    )
+    -- npairs.add_rules(
+    --   { Rule("<", ">") }
+    -- )
   end,
 }
