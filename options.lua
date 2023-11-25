ecbaf6dff7a89d6e50503ca0fbaccbec10bd5a79
Cangsong Wang <knairwang@outlook.com>
2023-11-25 11:22:06 +0800

    update autopairs

--- options.lua
+++ options.lua
@@ -3,7 +3,7 @@
 if vim.g.neovide then
   -- Put anything you want to happen only in Neovide here
   -- vim.o.guifont = "Hack Nerd Font Mono,Symbols Nerd Font Mono:h12"
-  vim.g.neovide_scroll_animation_length = 0.75
+  vim.g.neovide_scroll_animation_length = 0.25
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_refresh_rate = 60
   vim.g.neovide_refresh_rate_idle = 5
--- plugins/autopairs.lua
+++ plugins/autopairs.lua
@@ -1,6 +1,17 @@
 return {
   "windwp/nvim-autopairs",
   opts = {
-    ignored_next_char = "[%w%.%(%{%[]"
-  }
+    ignored_next_char = "[%w%.%(%{%[]",
+  },
+  config = function(plugin, opts)
+    require "plugins.configs.nvim-autopairs"(plugin, opts)
+
+    local npairs = require "nvim-autopairs"
+    local Rule = require "nvim-autopairs.rule"
+    local cond = require "nvim-autopairs.conds"
+
+    npairs.add_rules(
+      { Rule("<", ">") }
+    )
+  end,
 }
