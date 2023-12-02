7255b1c907ca70d9415f363687584787cd69fc6a
Cangsong Wang <knairwang@outlook.com>
2023-12-02 16:32:00 +0800

    update autopairs

--- plugins/autopairs.lua
+++ plugins/autopairs.lua
@@ -3,18 +3,29 @@ return {
   opts = {
     ignored_next_char = "[%w%.%(%{%[]",
     fast_wrap = {
-      chars = { "{", "[", "(", '"', "'" },
+      chars = { "{", "[", "(", '"', "'", "`" },
     }
   },
   config = function(plugin, opts)
     require "plugins.configs.nvim-autopairs"(plugin, opts)
 
-    -- local npairs = require "nvim-autopairs"
-    -- local Rule = require "nvim-autopairs.rule"
-    -- local cond = require "nvim-autopairs.conds"
+    local npairs = require "nvim-autopairs"
+    local Rule = require "nvim-autopairs.rule"
+    local cond = require "nvim-autopairs.conds"
 
-    -- npairs.add_rules(
-    --   { Rule("<", ">") }
-    -- )
+    npairs.add_rules(
+      {
+        Rule("<", ">", "rust")
+          :with_pair(cond.before_text("::"))
+          :with_cr(cond.none())
+      }
+    )
+    npairs.add_rules(
+      {
+        Rule("<", ">", "rust")
+          :with_pair(cond.before_regex("%w"))
+          :with_cr(cond.none())
+      }
+    )
   end,
 }
