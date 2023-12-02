d3f3a8e97d0c7a7e6cb3916d650a134ceb25dea1
Cangsong Wang <knairwang@outlook.com>
2023-12-02 10:50:29 +0800

    config cmp

---
+++ plugins/cmp.lua
@@ -0,0 +1,77 @@
+local cmp = require "cmp"
+local types = require("cmp.types")
+
+-- Text = 1,
+-- Method = 2,
+-- Function = 3,
+-- Constructor = 4,
+-- Field = 5,
+-- Variable = 6,
+-- Class = 7,
+-- Interface = 8,
+-- Module = 9,
+-- Property = 10,
+-- Unit = 11,
+-- Value = 12,
+-- Enum = 13,
+-- Keyword = 14,
+-- Snippet = 15,
+-- Color = 16,
+-- File = 17,
+-- Reference = 18,
+-- Folder = 19,
+-- EnumMember = 20,
+-- Constant = 21,
+-- Struct = 22,
+-- Event = 23,
+-- Operator = 24,
+-- TypeParameter = 25,
+
+local function prioritize(kind, higher)
+  return function(entry1, entry2)
+    if entry1:get_kind() == kind then return higher end
+    if entry2:get_kind() == kind then return not higher end
+  end
+end
+
+local function lexicographical(entry1, entry2)
+  if entry1:get_word() and entry2:get_word() then
+    local diff = vim.stricmp(entry1:get_word(), entry2:get_word())
+    if diff < 0 then
+      return true
+    elseif diff > 0 then
+      return false
+    end
+  end
+  return nil
+end
+
+return {
+  "hrsh7th/nvim-cmp",
+  opts = {
+    sources = cmp.config.sources {
+      { name = "nvim_lsp", priority = 1000 },
+      { name = "luasnip",  priority = 750 },
+      -- { name = "buffer", priority = 500 },
+      { name = "path",     priority = 250 },
+    },
+    sorting = {
+      priority_weight = 2,
+      comparators = {
+        cmp.config.compare.exact,
+        prioritize(types.lsp.CompletionItemKind.Variable , true),
+        prioritize(types.lsp.CompletionItemKind.Snippet, false),
+        lexicographical,
+        -- cmp.config.compare.sort_text,
+        cmp.config.compare.order,
+        -- cmp.config.compare.kind,
+        -- cmp.config.compare.score,
+        -- cmp.config.compare.length,
+        -- cmp.config.compare.offset,
+        -- cmp.config.compare.scopes,
+        -- cmp.config.compare.recently_used,
+        -- cmp.config.compare.locality,
+      },
+    },
+  }
+}
