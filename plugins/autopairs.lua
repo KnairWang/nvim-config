8fd46462bb77d36624cd62af22afca4af4bacdcb
Cangsong Wang <cwang40@slb.com>
2023-12-14 10:47:12 +0800

    fix autopairs config

--- plugins/autopairs.lua
+++ plugins/autopairs.lua
@@ -13,12 +13,16 @@ return {
     local Rule = require "nvim-autopairs.rule"
     local cond = require "nvim-autopairs.conds"
 
+    local function move_with_end_pair(opts)
+      return opts.char == opts.rule.end_pair
+    end
+
     npairs.add_rules(
       {
         Rule("<", ">", "rust")
             :with_pair(cond.before_text("::"))
             :with_cr(cond.none())
-            :with_move(cond.done())
+            :with_move(move_with_end_pair)
       }
     )
     npairs.add_rules(
@@ -26,7 +30,17 @@ return {
         Rule("<", ">", "rust")
             :with_pair(cond.before_regex("%w"))
             :with_cr(cond.none())
-            :with_move(cond.done())
+            :with_move(move_with_end_pair)
+      }
+    )
+
+    npairs.add_rules(
+      {
+        Rule("<", ">", "rust")
+            :with_pair(cond.before_text("<"))
+            :with_pair(cond.after_text(">"))
+            :with_cr(cond.none())
+            :with_move(move_with_end_pair)
       }
     )
   end,
